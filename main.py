e81369e26d577377c1902a75461d093b67864932
gugu256 <gugu256@mail.com>
2023-10-22 13:00:39 +0000

    cookies system + fixed leaderboard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# remember to use the in operator and not the == operator!
-
-from flask import Flask, request, render_template
+from os import truncate
+from flask import Flask, request, render_template, make_response
 from db import SimpleDB
 import random
 
@@ -21,20 +20,22 @@ def getwords(num):
         _keys.pop(n)
     return chosenwords
 
+def generate(start, end):
+    return list(range(start, end-1, -1))
+
 @app.route('/') # Homepage
 def home():
     with open("templates/index.html", "r") as f:
         htmlcode = f.read()
 
     leaderboard = scores.get()
-    sorted_keys = list(reversed(sorted(leaderboard)))
+    sorted_keys = sorted(leaderboard)
     
-    if len(sorted_keys) >= 1:
-        for i in range(0, len(sorted_keys)):
-            key = sorted_keys[i]
-            n = i+1
-            htmlcode += f"""<p>{n} : <u>{key}</u> (score : {leaderboard[key]})"""
-
+    n = 0
+    
+    for key in sorted_keys:
+        n += 1
+        htmlcode += f"""<p>{n}. <u>{key}</u> (score : {leaderboard[key]})"""
     htmlcode += """
                 </body>
                 </html>
@@ -47,16 +48,28 @@ def test():
         htmlcode = f.read()
     testwords = getwords(5)
 
+    pseudo = request.cookies.get('pseudo')
+
     for word in testwords:
         htmlcode += f"""
                     <label for="{word}">{word} : </label>
                     <input type="text" name="{word}"><br><br>
                     """
-        
+
+    if pseudo != "" and pseudo != None:
+        htmlcode += f"""
+                    <br>
+                    <label for="pseudo">Pseudo pour le classement (si tu ne veux pas être dans le classement, ne mets rien) : </label>
+                    <input type="text" name="pseudo" value="{pseudo}">
+                    """
+    else:
+        htmlcode += """
+                    <br>
+                    <label for="pseudo">Pseudo pour le classement (si tu ne veux pas être dans le classement, ne mets rien) : </label>
+                    <input type="text" name="pseudo">
+                    """
+    
     htmlcode += """
-                <br>
-                <label for="pseudo">Pseudo pour le classement (si tu ne veux pas être dans le classement, ne mets rien) : </label>
-                <input type="text" name="pseudo">
                 <br>
                 <br>
                 <br>
@@ -97,7 +110,7 @@ def results():
                 </html>
                 """
     
-    if pseudo != "" and len(pseudo) > 1:
+    if pseudo != "" and len(pseudo) > 1 and score >= 1:
         if pseudo not in scores.getkeys():
             scores.insert(pseudo, score)
         elif pseudo in scores.getkeys():
@@ -105,7 +118,8 @@ def results():
             newscore += score
             scores.set(pseudo, newscore)
 
+    resp = make_response(htmlcode)
+    resp.set_cookie("pseudo", pseudo)
+    return resp
 
-    return htmlcode
-
-app.run(host='0.0.0.0',port=8080)
+app.run(host='0.0.0.0')
